84cd3a11a71973c41a379104d52f924ef22b8f7c
Paul <frall712@gmail.com>
2016-06-13 22:56:43 +0300

    Initial project

---
+++ articles_files/qw.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 9:08:46 янв 8, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:32:00 янв 30, 2016.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+QW_C={
+    serverUrl: '/web/20140108090846/http://lineless.com',
+    staticUrl: '/web/20140108090846/http://lineless.com/f'
+};
+QW_Cf={
+    Time12: 2,
+    Time24: 1
+};(function (){
+    // загружаем основной скрипт
+    var sc = document.createElement('script');
+    sc.type = 'text/javascript'; 
+    sc.async = true;
+    sc.src = QW_C.staticUrl+'/js/qw_widget.js';
+    var s = document.getElementsByTagName('head')[0]; 
+    s.appendChild(sc);
+})();
